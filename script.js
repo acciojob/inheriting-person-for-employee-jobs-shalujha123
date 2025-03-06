2838f9607b911f57df05bf870857d00ee0313e42
Shalu <88194182+shalujha123@users.noreply.github.com>
2025-03-06 16:39:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ class Employee extends Person {
     }
 
     jobGreet() {
-        console.log(`Hello, my name is ${this.name} and I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
     }
 }
 
